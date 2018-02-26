cdc35246fecf1a19abb57e7e83b48533215d5b75
Frank Congson <fcongson@gmail.com>
2018-02-26 19:50:53 +1300

    Add bear model

--- handler.js
+++ handler.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const connectToDatabase = require('./db');
+const Bear = require('./models/Bear');
 
 module.exports.create = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
