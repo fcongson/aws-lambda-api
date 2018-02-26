03b77491a0dcaa8f49102d8b248e098df9569c17
Frank Congson <fcongson@gmail.com>
2018-02-26 19:42:28 +1300

    Add functions

--- handler.js
+++ handler.js
@@ -1,16 +1,111 @@
-'use strict';
+"use strict";
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+module.exports.create = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
 
-  callback(null, response);
+  connectToDatabase().then(() => {
+    Bear.create(JSON.parse(event.body))
+      .then(bear =>
+        callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(bear)
+        })
+      )
+      .catch(err =>
+        callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { "Content-Type": "text/plain" },
+          body: "Could not create bear."
+        })
+      );
+  });
+};
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+module.exports.getOne = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  connectToDatabase().then(() => {
+    Bear.findById(event.pathParameters.id)
+      .then(bear =>
+        callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(bear)
+        })
+      )
+      .catch(err =>
+        callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { "Content-Type": "text/plain" },
+          body: "Could not fetch bear."
+        })
+      );
+  });
+};
+
+module.exports.getAll = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  connectToDatabase().then(() => {
+    Bear.find()
+      .then(bears =>
+        callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(bears)
+        })
+      )
+      .catch(err =>
+        callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { "Content-Type": "text/plain" },
+          body: "Could not fetch bears."
+        })
+      );
+  });
+};
+
+module.exports.update = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  connectToDatabase().then(() => {
+    Bear.findByIdAndUpdate(event.pathParameters.id, JSON.parse(event.body), {
+      new: true
+    })
+      .then(bear =>
+        callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(bear)
+        })
+      )
+      .catch(err =>
+        callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { "Content-Type": "text/plain" },
+          body: "Could not fetch bears."
+        })
+      );
+  });
+};
+
+module.exports.delete = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  connectToDatabase().then(() => {
+    Bear.findByIdAndRemove(event.pathParameters.id)
+      .then(bear =>
+        callback(null, {
+          statusCode: 200,
+          body: JSON.stringify({
+            message: "Removed bear with id: " + bear._id,
+            bear: bear
+          })
+        })
+      )
+      .catch(err =>
+        callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { "Content-Type": "text/plain" },
+          body: "Could not fetch bears."
+        })
+      );
+  });
 };
